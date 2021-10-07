eba1a0927a85d05db9a75f9f652770085aa94035
alex <alex.haight@gmail.com>
2021-10-06 20:10:55 -0500

    spent a while making a make file

---
+++ includes/copter.h
@@ -0,0 +1,1 @@
+int copterStart(void);
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <copter.h>
+
+int main() {
+	copterStart();
+	return 0;
+}
+
+int copterStart(void) {
+  printf("We are playing copter");
+  return 0;
+}
