c361fa60d3733c8fe089b6965ad464e65d07f7b3
alex <alex.haight@gmail.com>
2021-10-07 14:08:40 -0500

    adding SDL lib

--- src/main.c
+++ src/main.c
@@ -1,12 +1,44 @@
+#include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
 #include <copter.h>
+#define SDL_MAIN_HANDLED
+
+#include <SDL2/SDL.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef float f32;
+typedef double f64;
+
+#define WIDTH 400
+#define HEIGHT 750
+
+enum GamePhase{
+  GAME_PLAY
+};
+
+struct Copter{
+  s32 xPos;
+  s32 yPos;
+  s32 accel;
+};
+
+struct GameState{
+  u8 gameBoard[WIDTH][HEIGHT];
+  // Copter copter;
+
+  // GamePhase phase;
+};
 
 int main() {
-	copterStart();
-	return 0;
-}
-
-int copterStart(void) {
   printf("We are playing copter");
-  return 0;
+	return 0;
 }
